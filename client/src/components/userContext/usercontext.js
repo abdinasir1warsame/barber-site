e2dc277d4b698773956431ae029a934bb6d19f73
abdinasir1993 <abdinasir_jirde@hotmail.com>
2024-04-30 23:56:00 +0100

    .

--- client/src/components/userContext/usercontext.js
+++ client/src/components/userContext/usercontext.js
@@ -9,7 +9,7 @@ export function UserContextProvider({ children }) {
   useEffect(() => {
     if (!user) {
       axios
-        .get('/profile')
+        .get('/profile', { credentials: 'include' }) // Add credentials option here
         .then(({ data }) => {
           console.log(data);
           setUser(data);
