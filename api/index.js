cf36db1c0805ff542386149806d9f7b01e42d4f6
abdinasir1993 <abdinasir_jirde@hotmail.com>
2024-04-30 16:57:00 +0100

    .

--- api/index.js
+++ api/index.js
@@ -70,18 +70,18 @@ app.post('/login', async (req, res) => {
 });
 
 // Protected route using JWT
-// app.get('/profile', (req, res) => {
-//   const { token } = req.cookies;
-//   if (token) {
-//     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-//       if (err) throw err;
-//       const { name, email, _id } = await User.findById(userData.id);
-//       res.json({ name, email, _id });
-//     });
-//   } else {
-//     res.json(null);
-//   }
-// });
+app.get('/profile', (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
+    });
+  } else {
+    res.json(null);
+  }
+});
 app.post('/bookings', async (req, res) => {
   const { date, time, service, barberName } = req.body;
   try {
